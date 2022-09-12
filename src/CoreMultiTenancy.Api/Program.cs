e0cc9b6d1085e288b03092075a95a87403a2c03a
Dustin Cotten <dustin.cotten@avolanta.com>
2022-09-11 21:15:33 -0700

    api updated ef core accessing

--- src/CoreMultiTenancy.Api/Program.cs
+++ src/CoreMultiTenancy.Api/Program.cs
@@ -1,16 +1,21 @@
+using CoreMultiTenancy.Api;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+    .Enrich.FromLogContext());
 
-var app = builder.Build();
+var app = builder.ConfigureServices();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
 }
 
-
-app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 app.UseAuthorization();
-app.MapControllers();
 
 app.Run();
---
+++ src/CoreMultiTenancy.Api/ServiceExtensions.cs
@@ -0,0 +1,51 @@
+using Cmt.Protobuf;
+using CoreMultiTenancy.Api.Data;
+using CoreMultiTenancy.Api.Interfaces;
+using CoreMultiTenancy.Api.Tenancy;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CoreMultiTenancy.Api;
+
+internal static class ServiceExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<TenantedDbContext>();
+
+        builder.Services.AddControllers();
+        builder.Services.AddApiVersioning();
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddAuthentication("Bearer")
+            .AddJwtBearer("Bearer", o =>
+            {
+                o.Authority = "https://localhost:5100";
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                };
+            });
+
+        builder.Services.AddAuthorization(o =>
+        {
+            o.AddPolicy("ApiScope", p =>
+            {
+                p.RequireAuthenticatedUser();
+                p.RequireClaim("scope", "testapi");
+            });
+        });
+
+        builder.Services.AddScoped<ITenantProvider, RouteDataTenantProvider>();
+        builder.Services.AddGrpc();
+        builder.Services.AddGrpcClients();
+
+        return builder.Build();
+    }
+
+    public static void AddGrpcClients(this IServiceCollection sc)
+    {
+        sc.AddGrpcClient<PermissionAuthorize.PermissionAuthorizeClient>(o =>
+        {
+            o.Address = new Uri("https://localhost:5100");
+        });
+    }
+}
