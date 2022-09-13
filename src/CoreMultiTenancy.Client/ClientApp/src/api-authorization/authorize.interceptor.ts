951525a8dda28d9b3f6c7cf2ca575ae0da3072a1
Dustin Cotten <dustin.cotten@avolanta.com>
2022-09-12 21:33:55 -0700

    fixed cors issue

--- src/CoreMultiTenancy.Client/ClientApp/src/api-authorization/authorize.interceptor.ts
+++ src/CoreMultiTenancy.Client/ClientApp/src/api-authorization/authorize.interceptor.ts
@@ -19,7 +19,7 @@ export class AuthorizeInterceptor implements HttpInterceptor {
   // and adds it to the request in case it's targeted at the same origin as the
   // single page application.
   private processRequestWithToken(token: string | null, req: HttpRequest<any>, next: HttpHandler) {
-    if (!!token && this.isSameOriginUrl(req)) {
+    if (!!token && req.url.startsWith("https://localhost:5100")) {
       req = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
@@ -28,27 +28,5 @@ export class AuthorizeInterceptor implements HttpInterceptor {
     }
 
     return next.handle(req);
-  }
-
-  private isSameOriginUrl(req: any) {
-    // It's an absolute url with the same origin.
-    if (req.url.startsWith(`${window.location.origin}/`)) {
-      return true;
-    }
-
-    // It's a protocol relative url with the same origin.
-    // For example: //www.example.com/api/Products
-    if (req.url.startsWith(`//${window.location.host}/`)) {
-      return true;
-    }
-
-    // It's a relative url like /api/Products
-    if (/^\/[^\/].*/.test(req.url)) {
-      return true;
-    }
-
-    // It's an absolute or protocol relative url that
-    // doesn't have the same origin.
-    return false;
   }
 }
