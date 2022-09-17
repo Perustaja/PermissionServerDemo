2caf934dba3f42caef2b652f5ee738e2dd26fcc2
Dustin Cotten <dustin.cotten@avolanta.com>
2022-09-16 21:06:44 -0700

    initial full aircraft page

--- src/CoreMultiTenancy.Client/ClientApp/src/app/sidebar/sidebar.component.ts
+++ src/CoreMultiTenancy.Client/ClientApp/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { faPlaneDeparture, faUsers, faSitemap, faCog, faBuilding } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
@@ -6,10 +7,18 @@ import { faPlaneDeparture, faUsers, faSitemap, faCog, faBuilding } from '@fortaw
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   faPlaneDeparture = faPlaneDeparture;
   faUsers = faUsers;
   faSitemap = faSitemap;
   faCog = faCog;
   faBuilding = faBuilding;
+
+  constructor(private route: ActivatedRoute) {
+    route.url
+  }
+
+  ngOnInit() {
+
+  }
 }
