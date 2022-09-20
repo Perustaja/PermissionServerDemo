e12fd8d5c1813bd8d64c127e5b038d338ff9ee91
Dustin Cotten <dustin.cotten@avolanta.com>
2022-09-19 19:07:33 -0700

    aircraft page done

--- src/CoreMultiTenancy.Client/ClientApp/src/api-authorization/login-menu/login-menu.component.ts
+++ src/CoreMultiTenancy.Client/ClientApp/src/api-authorization/login-menu/login-menu.component.ts
@@ -1,16 +1,24 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { map, Observable } from 'rxjs';
+import { AuthorizeService } from '../authorize.service';
 
 @Component({
   selector: 'app-login-menu',
   templateUrl: './login-menu.component.html',
   styleUrls: ['./login-menu.component.css']
 })
-export class LoginMenuComponent{
+export class LoginMenuComponent implements OnInit {
   faUserCircle = faUserCircle;
   idpBaseUrl: string;
+  userName?: Observable<string | null | undefined>;
 
-  constructor(@Inject('IDP_BASE_URL') idpBaseUrl: string) {
+  constructor(private authorizeSvc: AuthorizeService,
+    @Inject('IDP_BASE_URL') idpBaseUrl: string) {
     this.idpBaseUrl = idpBaseUrl;
+  }
+
+  ngOnInit() {
+    this.userName = this.authorizeSvc.getUser().pipe(map(u => u && u.name));
   }
 }
